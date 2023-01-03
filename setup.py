134b6c91be331e7bc329189a725b1fee4ca68868
1ucky40nc3 <louisnwendler@gmail.com>
2023-01-03 20:51:08 +0000

    setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="jaxperiments",
+    version="0.0.1",
+    description="some machine learning experiments with jax",
+    author="Louis Wendler",
+    author_email="louisnwendler@gmail.com",
+    url="https://github.com/1ucky40nc3/jaxperiments",
+    license="MIT License",
+    packages=["jaxperiments"]
+)
